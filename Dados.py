020b19f26ca7ecfb152888c9724e85a3129ee649
Ester <ester.ribeiro10101@gmail.com>
2018-10-12 00:13:48 -0300

    Base de dados

---
+++ Dados.py
@@ -0,0 +1,49 @@
+avaliacoes = {'Lucas':
+                  {'Neuromancer': 2.5,
+                   'Frankenstein': 3.5,
+                   'Nineteen Eighty-Four': 3.0,
+                   'The Hitchhiker''s Guide to the Galaxy': 3.5,
+                   'Androides Sonham com Ovelhas Elétricas?': 2.5,
+                   'Fahrenheit 451': 3.0},
+
+              'Ester':
+                  {'Neuromancer': 3.0,
+                   'Frankenstein': 3.5,
+                   'Nineteen Eighty-Four': 1.5,
+                   'The Hitchhiker''s Guide to the Galaxy': 5.0,
+                   'Androides Sonham com Ovelhas Elétricas?': 3.0,
+                   'Fahrenheit 451': 3.5},
+
+              'Douglas':
+                  {'Neuromancer': 2.5,
+                   'Frankenstein': 3.0,
+                   'Nineteen Eighty-Four': 3.5,
+                   'The Hitchhiker''s Guide to the Galaxy': 4.0},
+
+              'Gabryell':
+                  {'O Ultimato BourneAndroides Sonham com Ovelhas Elétricas?': 3.5,
+                   'Nineteen Eighty-Four': 3.0,
+                   'Frankenstein': 4.5,
+                   'Neuromancer': 4.0,
+                   'Fahrenheit 451': 2.5},
+
+              'Humberto':
+                  {'Frankenstein': 3.0,
+                   'Nineteen Eighty-Four': 4.0,
+                   'Neuromancer': 2.0,
+                   'The Hitchhiker''s Guide to the Galaxy': 3.0,
+                   'Androides Sonham com Ovelhas Elétricas?': 3.0,
+                   'Fahrenheit 451': 2.0},
+
+              'Diego':
+                  {'Nineteen Eighty-Four': 3.0,
+                   'Frankenstein': 4.0,
+                   'Neuromancer': 3.0,
+                   'The Hitchhiker''s Guide to the Galaxy': 5.0,
+                   'Fahrenheit 451': 3.5},
+
+              'Patrick':
+                  {'Nineteen Eighty-Four': 4.5,
+                   'The Hitchhiker''s Guide to the Galaxy': 1.0,
+                   'Neuromancer': 4.0}
+              }
