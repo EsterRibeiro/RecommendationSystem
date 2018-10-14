171ea2703d630104707ae9efdd6ef5eb2ace6196
Ester Ribeiro <ester.ribeiro10101@gmail.com>
2018-10-14 00:51:29 -0300

    Aplicacao da funcao euclidiana, para ver a distância de classificação de livros entre usuários

--- Dados.py
+++ Dados.py
@@ -47,3 +47,21 @@ avaliacoes = {'Lucas':
                    'The Hitchhiker''s Guide to the Galaxy': 1.0,
                    'Neuromancer': 4.0}
               }
+
+#função euclidiana
+
+from math import sqrt
+
+def euclidiana(usuario_1, usuario_2):
+    livros_comum = {} #verifica se há avaliacoes do mesmo filme entre os dois usuários
+
+    for item in avaliacoes[usuario_1]: #busca os livros que o usuário leu
+        if item in avaliacoes[usuario_2]: livros_comum[item] = 1 #verifica se os livros tbm estão no user 2
+
+    if len(livros_comum == 0): return 0 #tamanho da lista == 0 (não tem livros em comum)
+
+    soma = sum([pow(avaliacoes[usuario_1][item] - avaliacoes[usuario_2][item], 2) #funcao euclidiana
+
+        for item in avaliacoes[usuario_1] if item in avaliacoes[usuario_2]]) #pega todos os itens dentro dos dois usuários
+
+    return 1/(1 + sqrt(soma)) #retorna o valor em porcentagem
