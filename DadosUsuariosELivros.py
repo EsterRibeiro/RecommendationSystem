f5780aec4acd8dc8d4ecac682b1f172761eb9f05
Ester <ester.ribeiro10101@gmail.com>
2018-11-18 23:11:11 -0200

    Inclusão da variável 'base', para utilizar os dois métodos de avaliação.

--- DadosUsuariosELivros.py
+++ DadosUsuariosELivros.py
@@ -101,45 +101,45 @@ avaliacoesLivro = {'Neuromancer':
 from math import sqrt
 import matplotlib.pyplot as plt
 
-def euclidiana(usuario_1, usuario_2):
+def euclidiana(base, usuario_1, usuario_2):
     livros_comum = {} #verifica se há avaliacoes do mesmo filme entre os dois usuários
 
-    for item in avaliacoes[usuario_1]: #busca os livros que o usuário leu
-        if item in avaliacoes[usuario_2]: livros_comum[item] = 1 #verifica se os livros tbm estão no user 2
+    for item in base[usuario_1]: #busca os livros que o usuário leu
+        if item in base[usuario_2]: livros_comum[item] = 1 #verifica se os livros tbm estão no user 2
 
     if len(livros_comum) == 0: return 0 #tamanho da lista == 0 (não tem livros em comum)
 
-    soma = sum([pow(avaliacoes[usuario_1][item] - avaliacoes[usuario_2][item], 2) #funcao euclidiana
+    soma = sum([pow(base[usuario_1][item] - base[usuario_2][item], 2) #funcao euclidiana
 
-        for item in avaliacoes[usuario_1] if item in avaliacoes[usuario_2]]) #pega todos os itens dentro dos dois usuários
+        for item in base[usuario_1] if item in base[usuario_2]]) #pega todos os itens dentro dos dois usuários
 
     return 1/(1 + sqrt(soma)) #retorna o valor e porcentagem
 
 #lista de usuários similares
 
-def getSimilares(usuario):
+def getSimilares(base, usuario):
     similaridade = [(euclidiana(usuario, outro), outro) #funcao euclidiana 
-                    for outro in avaliacoes if outro != usuario] #chama a lista de usuarios sem comparar com ele mesmo
+                    for outro in base if outro != usuario] #chama a lista de usuarios sem comparar com ele mesmo
     similaridade.sort()
     similaridade.reverse() 
     return similaridade # irá retornar a similaridade em ordem decrescente
 
 #função de recomendação
 
-def getRecomendacoes(usuario): #usuario a ser recomendado
+def getRecomendacoes(base, usuario): #usuario a ser recomendado
     totais = {} #soma da multiplicação da similaridade pela nota do usuário em relação ao livro
     somaSimilaridade= {} #soma da similaridade dos filmes
 
-    for outro in avaliacoes:
+    for outro in base:
         if outro == usuario: continue #não pode comparar a ele mesmo
-        similaridade = euclidiana(usuario, outro)
+        similaridade = euclidiana(base, usuario, outro)
 
         if similaridade <= 0: continue #deve haver similaridade
 
-        for item in avaliacoes[outro]: #percorre os itens
-            if item not in avaliacoes[usuario]: #o item não deve estar na lista de filmes que o usuário já assistiu
+        for item in base[outro]: #percorre os itens
+            if item not in base[usuario]: #o item não deve estar na lista de filmes que o usuário já assistiu
                 totais.setdefault(item, 0) # acumulação da similaridade x a nota dada pelo usuário
-                totais[item] += avaliacoes[outro][item] * similaridade
+                totais[item] += base[outro][item] * similaridade
                 somaSimilaridade.setdefault(item, 0)
                 somaSimilaridade[item] += similaridade
     rankings = [(total / somaSimilaridade[item], item) for item, total in totais.items()]
